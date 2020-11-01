2ff31b6636485a83812c2ce56681cfddac7e0490
LeonardoLeiteMeira <42722914+LeonardoLeiteMeira@users.noreply.github.com>
2020-11-01 20:57:20 -0300

    finished first version

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+const { Console } = require("console");
+
 module.exports = {
     getApd() {
         file = JSON.parse(require("fs").readFileSync(process.argv[2])).ap;
@@ -25,22 +27,96 @@ module.exports = {
         }
         return transitions;
     },
+    makeTransition() {},
+    hasTransitionsToMake(char, transitions, currentState, stack) {
+        for (let index = 0; index < transitions.length; index++) {
+            if (
+                transitions[index].from === currentState &&
+                (transitions[index].input === char ||
+                    transitions[index].input === "#") &&
+                (transitions[index].pop === stack[stack.length - 1] ||
+                    transitions[index].pop === "#")
+            ) {
+                return true;
+            }
+        }
+        return false;
+    },
     checkApd(word) {
         const apd = this.getApd();
         const stack = [];
         const transitions = this.getTransitions(apd);
 
         currentState = apd.initialState;
+        let isValid = false;
+        let index = 0;
 
-        for (i = 0; i < word.length; i++) {
-            console.log(word[i]);
+        word = word.replace("#", "");
 
-            /* magica */
+        while (
+            this.hasTransitionsToMake(
+                word.charAt(index) ? word.charAt(index) : "",
+                transitions,
+                currentState,
+                stack
+            )
+        ) {
+            let lambda = true;
+            [...transitions].forEach((transition) => {
+                if (
+                    transition.from === currentState &&
+                    transition.input === word.charAt(index) &&
+                    (transition.pop === stack[stack.length - 1] ||
+                        transition.pop === "#")
+                ) {
+                    currentState = transition.to;
+
+                    const size = transition.push.length;
+                    for (let i = size - 1; i >= 0; i--) {
+                        transition.push.charAt(i) != "#" &&
+                            stack.push(transition.push.charAt(i));
+                    }
+                    transition.pop !== "#" && stack.pop();
+                    index++;
+                    lamba = false;
+                }
+            });
+            if (lambda) {
+                [...transitions].forEach((transition) => {
+                    if (
+                        transition.input === "#" &&
+                        (transition.pop === stack[stack.length - 1] ||
+                            transition.pop === "#")
+                    ) {
+                        currentState = transition.to;
+                        const size = transition.push.length;
+                        for (let idx = size - 1; idx >= 0; idx--) {
+                            transition.push.charAt(idx) != "#" &&
+                                stack.push(transition.push.charAt(idx));
+                        }
+                        transition.pop !== "#" && stack.pop();
+                    }
+                });
+            }
         }
+        if (
+            stack.length === 0 &&
+            index === word.length &&
+            apd.finalStates.includes(currentState)
+        ) {
+            isValid = true;
+        }
+
+        return isValid;
     },
 
     checkWord(word) {
-        this.checkApd(word);
+        let isWordValid = this.checkApd(word);
+        if (isWordValid) {
+            console.log("Sim");
+        } else {
+            console.log("Não");
+        }
         readInput();
     },
 };
